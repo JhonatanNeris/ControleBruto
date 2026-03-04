038437db7d0a567819ff3a4fed390aacfa1c9869
Jhonatan <jhonatansnx@gmail.com>
2026-03-04 10:52:37 -0300

    Adicionado relatórios por categoria

--- ControleBruto/Controllers/ReportsController.cs
+++ ControleBruto/Controllers/ReportsController.cs
@@ -4,6 +4,7 @@ using ControleBruto.Models.Enums;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 
 namespace ControleBruto.Controllers;
 
@@ -49,8 +50,8 @@ public class ReportsController : ControllerBase
         var netWorth = await _context.Transactions
             .AsNoTracking()
             .Where(t => t.UserId == userId)
-            .SumAsync(t => (t.Type == TransactionType.Income ?  t.AmountCents : 
-                t.Type == TransactionType.Expense ? - t.AmountCents : 0));
+            .SumAsync(t => (t.Type == TransactionType.Income ? t.AmountCents :
+                t.Type == TransactionType.Expense ? -t.AmountCents : 0));
 
         return Ok(new
         {
@@ -59,6 +60,63 @@ public class ReportsController : ControllerBase
             MonthlyExpense = totalExpense,
             MonthlyBalance = totalIncome - totalExpense,
             TotalNetWorth = netWorth
-        });      
+        });
+    }
+
+    [HttpGet]
+    [Route("by-category")]
+    public async Task<IActionResult> GetReportByCategory([FromQuery] int month, [FromQuery] int year, [FromQuery] TransactionType type)
+    {
+        var userId = User.GetUserId();
+        var startDate = new DateTime(year, month, 1);
+        var endDate = startDate.AddMonths(1);
+
+        // Só faz sentido agrupar por categoria para Income ou Expense
+        if (type != TransactionType.Expense && type != TransactionType.Income)
+            return BadRequest(new { message = "type deve ser Expense ou Income." });
+
+        var transactions = await _context.Transactions
+            .AsNoTracking()
+            .Where(t => t.UserId == userId && t.Date >= startDate && t.Date < endDate && t.Type == type)
+            .GroupBy(t => t.CategoryId)
+            .Select(g => new
+            {
+                CategoryId = g.Key,
+                TotalCents = g.Sum(x => x.AmountCents),
+                Count = g.Count()
+            })
+            .ToListAsync();
+
+        var categoryIds = transactions
+          .Where(x => x.CategoryId.HasValue)
+          .Select(x => x.CategoryId!.Value)
+          .Distinct()
+          .ToList();
+
+        var categoryMap = await _context.Categories
+         .AsNoTracking()
+         .Where(c => c.UserId == userId && categoryIds.Contains(c.Id))
+         .Select(c => new { c.Id, c.Name })
+         .ToDictionaryAsync(x => x.Id, x => x.Name);
+
+
+        // 3) Monta resposta final com nomes
+        var result = transactions.Select(x => new
+        {
+            x.CategoryId,
+            CategoryName = x.CategoryId.HasValue && categoryMap.ContainsKey(x.CategoryId.Value)
+                    ? categoryMap[x.CategoryId.Value]
+                    : "Sem categoria",
+            x.TotalCents,
+            x.Count
+        });
+
+
+        return Ok(new
+        {
+            Period = new { month, year },    
+            Type = type.ToString(),
+            Result = result
+        });
     }
 }
